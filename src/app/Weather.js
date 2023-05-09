62ec56672521e334b2b3f8b8f541ce8b8a82b981
jotal96 <jotal96@gmail.com>
2023-05-09 09:59:39 +0100

    Metodos CRUD

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Lista from './Lista';
 import Detalhe from './Detalhe';
 
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 class Weather extends Component {
-  state = { listaWeather: [], dadosWeather: null }
+  state = { listaWeather: [], dadosWeather: null, startDate: new Date() }
 
   async componentDidMount() {
+    // this.removerPrevisao(1);
+    // this.atualizarPrevisao(5);
     this.buscarDadosIniciais();
   }
 
@@ -18,7 +24,7 @@ class Weather extends Component {
     fetch("https://localhost:7261/weatherforecast", requestOptions)
       .then(res => res.json())
       .then(result => this.setState({ listaWeather: result }))
-      .catch(error => console.log('error', error));
+      .catch(error => { console.log('error', error) });
   }
 
   buscarDetalhes(id) {
@@ -29,7 +35,7 @@ class Weather extends Component {
   }
 
   async criarPrevisao() {
-    let obj = {Date: new Date().toJSON(), TemperatureC:20, Summary: "Descrição genérica"};
+    let obj = { Date: this.state.startDate.toJSON(), TemperatureC: 20, Summary: "Descrição genérica" };
 
     var requestOptions = {
       method: 'POST',
@@ -47,17 +53,50 @@ class Weather extends Component {
       .catch(error => console.log('error', error));
   }
 
+  async atualizarPrevisao(id) {
+    let obj = { Date: new Date().toJSON(), TemperatureC: -20, Summary: "fRIO", Id: id };
+
+    var requestOptions = {
+      method: 'PUT',
+      redirect: 'follow',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(obj)
+    };
+
+    fetch("https://localhost:7261/weatherforecast/update", requestOptions)
+      .then(res => res.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  async removerPrevisao(id) {
+    var requestOptions = {
+      method: 'DELETE',
+      redirect: 'follow'
+    };
+
+    fetch("https://localhost:7261/weatherforecast/delete/" + id, requestOptions)
+      .then(res => res.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
 
 
   render() {
     if (this.state.dadosWeather === null) {
       return (
-        <Lista lista={this.state.listaWeather}
-          buscarDetalhes={(id) => { this.buscarDetalhes(id) }}
-        />
+        <>
+          <Lista lista={this.state.listaWeather}
+            buscarDetalhes={(id) => { this.buscarDetalhes(id) }}
+          />
+          <DatePicker selected={this.state.startDate} onChange={(date) => this.setState({startDate: date})} />
+        </>
       );
-    }else{
-      return <Detalhe dadosPrevisao={this.state.dadosWeather}/>;
+    } else {
+      return <Detalhe dadosPrevisao={this.state.dadosWeather} />;
     }
 
 
